1f889370f0f9971f178219a843419220fa28efa0
dcrawsh <dcrawsh@gmail.com>
2020-08-04 18:43:49 -0700

    create store

---
+++ front-end/src/actions/postActions.js
@@ -0,0 +1,10 @@
+export const fetchPosts = () => {
+    return (dispatch) => {
+      dispatch({ type: 'LOADING_POSTS'})
+      fetch('http://localhost:3000/api/v1/posts').then(response => {
+        return response.json()
+      }).then(responseJSON => {
+        dispatch({ type: 'ADD_POSTS', posts: responseJSON.data })
+      })
+    }
+  }
