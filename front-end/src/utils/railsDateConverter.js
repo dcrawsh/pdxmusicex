de2b665d9c0da440bdbf47a581715b1efea61b46
dcrawsh <dcrawsh@gmail.com>
2020-08-28 20:53:38 -0700

    Update railsDateConverter.js

--- front-end/src/utils/railsDateConverter.js
+++ front-end/src/utils/railsDateConverter.js
@@ -1,5 +1,5 @@
 export default function railsDateConverter (inputFormat) {
     function pad(s) { return (s < 10) ? '0' + s : s; }
     var d = new Date(inputFormat);
-    return [pad(d.getDate()), pad(d.getMonth()+1), d.getFullYear()].join('/');
+    return [pad(d.getMonth()+1), pad(d.getDate()), d.getFullYear()].join('/');
   }
